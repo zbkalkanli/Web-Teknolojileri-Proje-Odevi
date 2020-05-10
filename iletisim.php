fa8221944cb05dc15b3c303efd008757e4e73062
Zeynep Betul Kalkanli <z.b.kalkanli@gmail.com>
2020-05-10 19:06:30 +0300

    İletişim formuna eklemeler yapıldı.

--- iletisim.php
+++ iletisim.php
@@ -1,90 +1,105 @@
 <?php
-$isim=$_POST['isim'];
-$soyisim=$_POST['soyisim'];
-$konu=$_POST['konu'];
-$mesaj=$_POST['Mesajiniz'];
+$isim = $_POST['isim'];
+$soyisim = $_POST['soyisim'];
+$email = $_POST['email'];
+$tel = $_POST['telNo'];
+$konu = $_POST['konu'];
+$mesaj = $_POST['Mesajiniz'];
 ?>
 <!DOCTYPE html>
 <html>
+
 <head></head>
+
 <body>
     <?php
-        echo "Mesajınız alınmıştır. En kısa süre içerisinde tarafınıza dönüş sağlanacaktır.";
-   ?>
+    echo "Mesajınız alınmıştır. En kısa süre içerisinde tarafınıza dönüş sağlanacaktır.";
+    ?>
     <hr>
-	<table>
-		<tr>
-		<td>Adı Soyadı</td>
-		<td>:</td>
-		<td>
-		<?php
-		echo $_POST["isim"]."&nbsp;".$_POST["soyisim"];
-		?>
-		</td>
-		</tr>
-		
-		<tr>
-		<td>Cinsiyetiniz</td>
-		<td>:</td>
-		<td>
-		<?php
-        if(isset($_POST['cinsiyet']))
-		{
-		echo $_POST['cinsiyet'];
-		}
-		?>
-		</td>
-		</tr>
-		
-		<tr>
-		<td>Konu</td>
-		<td>:</td>
-		<td>
-		<?php
-		if($konu=='Şikayet')
-		{
-		echo "Şikayet";
-		}
-		if($konu=='Öneri')
-		{
-		echo "Öneri";
-		}
-		if($konu=='Bilgi Talebi')
-		{
-		echo "Bilgi Talebi";
-		}
-		if($konu=='Giriş Yapamıyorum')
-		{
-		echo "Giriş Yapamıyorum";
-        }
-        if($konu=='İstek')
-		{
-		echo "İstek";
-		}
-		if($konu=='Teşekkür')
-		{
-		echo "Teşekkür";
-        }
-        if($konu=='Diğer')
-		{
-		echo "Diğer";
-		}
-		?>
-		</td>
-		</tr>
-		
-		<tr>
-		<td>Mesajınız</td>
-		<td>:</td>
-		<td>
-		<?php
-		echo $mesaj;
-        ?>
-        
-		</td>
-		</tr>
-		
-	</table>
+    <table>
+        <tr>
+            <td>Adı Soyadı</td>
+            <td>:</td>
+            <td>
+                <?php
+                echo $_POST["isim"] . "&nbsp;" . $_POST["soyisim"];
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Cinsiyetiniz</td>
+            <td>:</td>
+            <td>
+                <?php
+                if (isset($_POST['cinsiyet'])) {
+                    echo $_POST['cinsiyet'];
+                }
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Email Adresiniz</td>
+            <td>:</td>
+            <td>
+                <?php
+                echo $email;
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Telefon Numaranız</td>
+            <td>:</td>
+            <td>
+                <?php
+                echo $tel;
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Konu</td>
+            <td>:</td>
+            <td>
+                <?php
+                if ($konu == 'Şikayet') {
+                    echo "Şikayet";
+                }
+                if ($konu == 'Öneri') {
+                    echo "Öneri";
+                }
+                if ($konu == 'Bilgi Talebi') {
+                    echo "Bilgi Talebi";
+                }
+                if ($konu == 'Giriş Yapamıyorum') {
+                    echo "Giriş Yapamıyorum";
+                }
+                if ($konu == 'İstek') {
+                    echo "İstek";
+                }
+                if ($konu == 'Teşekkür') {
+                    echo "Teşekkür";
+                }
+                if ($konu == 'Diğer') {
+                    echo "Diğer";
+                }
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Mesajınız</td>
+            <td>:</td>
+            <td>
+                <?php
+                echo $mesaj;
+                ?>
+
+            </td>
+        </tr>
+    </table>
 </body>
 
 </html>
